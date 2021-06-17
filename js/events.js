e299c22b12b76e185cac6e1f4057a078c97407c4
Signior-X <b19188@students.iitmandi.ac.in>
2021-06-18 01:41:29 +0530

    Added events sections

---
+++ js/events.js
@@ -0,0 +1,73 @@
+function createEventString(event) {
+  const defaultPoster = "https://e-cell-iitmandi.github.io/E-Cell_Website/posters/bob2.jpeg";
+
+  console.log("eve", event);
+
+  const eventString = `
+      <div class="event">
+        <div class="event-con">
+          <div class="flex-c">
+            <div class="event-img">
+              <img src="` + (event.posterUrl ? event.posterUrl : defaultPoster) + `">
+            </div>
+            <div class="event-desc">
+              <p>
+                ` + event.eventName + `
+              </p>
+            </div>
+            <div class="event-link">
+              <a href="">More Info</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      `;
+
+  console.log("He");
+
+  return eventString;
+}
+
+addEvents = (docsData) => {
+  // Pass the firebase docs here
+  const events = []
+  docsData.forEach(doc => {
+    events.push(doc.data());
+  });
+
+  let eventsRows = "";
+
+  for (let i = 0; i < 6; i += 3) {
+    // We will add three three pairs
+    const event1 = createEventString(events[i]);
+    const event2 = createEventString(events[i + 1]);
+    const event3 = createEventString(events[i + 2]);
+
+    console.log(event1);
+
+    const eventRow = `
+        <div class="events-row flex">
+          ` + event1 + event2 + event3 + `
+        </div>
+        `;
+
+    eventsRows += eventRow;
+  }
+
+  document.getElementById("events-container").innerHTML = eventsRows;
+}
+
+fetchDataFromFirebase = () => {
+  const db = firebase.firestore();
+  db.collection("contests").get()
+    .then((data) => {
+      addEvents(data);
+    }).catch((err) => {
+      // We will show some default events in this case
+      console.log("Event fetch error", err);
+    });
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  fetchDataFromFirebase();
+});
---
+++ js/firebase-config.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCgYIKmo-p9UXeN4vAe7QREClF14JLLo9I",
+  authDomain: "e-cell-iitmandi.firebaseapp.com",
+  databaseURL: "https://e-cell-iitmandi.firebaseio.com",
+  projectId: "e-cell-iitmandi",
+  storageBucket: "e-cell-iitmandi.appspot.com",
+  messagingSenderId: "93109878103",
+  appId: "1:93109878103:web:34970c428650f964c2d212",
+  measurementId: "G-E5DJP85NF0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
